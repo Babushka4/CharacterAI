a5649b9e7e3a4e61bcd224c4c4336cf10726ee9f
Denis <bobrov.deni2016@yandex.ru>
2023-07-29 12:44:41 +0300

    Initial commit

---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Table, Column, MetaData, Integer, String, ForeignKey
+
+metadata = MetaData()
+
+user = Table(
+    'user',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('username', String, nullable=False),
+    Column('name', String(length=100), nullable=False),
+    Column('surname', String(length=100), nullable=False),
+    Column('time', String(length=320), unique=True, index=True, nullable=False),
+
+    Column('character_id', ForeignKey('character.id')),
+)
+
+dialog = Table(
+    'dialog',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('request', String, nullable=False),
+    Column('response', String(length=320), nullable=False),
+
+    Column('user_id', ForeignKey('user.id'))
+)
+
+character = Table(
+    'character',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(length=320), unique=True, nullable=False)
+)
